8685e718f2ef07ca6ed99e544107d37ef5a8b5a0
tivanyaqlicia07 <tivanyaqlicia07@gmail.com>
2025-02-18 14:26:30 +0700

    menyelesaikan halaman about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,1 @@
+<h1>Ini sebenarnya kerjaan adjie</h1>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
